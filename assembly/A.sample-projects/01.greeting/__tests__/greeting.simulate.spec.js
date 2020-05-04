79fd4bb1ab915af287504730de91d3a2fdab0003
Sherif Abushadi <sherif@amgando.org>
2020-05-04 13:59:25 +0300

    remove cross contract call simulation tests from greeting sample

--- assembly/A.sample-projects/01.greeting/__tests__/greeting.simulate.spec.js
+++ assembly/A.sample-projects/01.greeting/__tests__/greeting.simulate.spec.js
@@ -212,30 +212,6 @@ describe("Greeting ", () => {
 
   describe("Cross-contract calls()", () => {
     test.todo("add cross contract call examples");
-
-    // let sentences: Account, words: Account, alice: Account;
-    // describe("cross contract calls", () => {
-    //   beforeEach(() => {
-    //     runtime = new Runtime();
-    //     alice = runtime.newAccount("alice");
-    //     sentences = runtime.newAccount("sentences", __dirname + "/../out/sentences.wasm");
-    //     words = runtime.newAccount("words.examples", __dirname + "/../out/words.wasm");
-    //   });
-
-    //   test("single promise", () => {
-    //     let res = alice.call_other("sentences", "reverseWordOne");
-    //     expect(res.return_data.text).toBe("elpmas")
-    //   });
-
-    //   test("promise + then with no arguments", () => {
-    //     let res  = alice.call_other("sentences", "reverseWordTwo");
-    //     expect(res.return_data).toBe(true)
-    //   });
-    //   test("promise + then with arguments", () => {
-    //     let res  = alice.call_other("sentences", "reverseWordThree");
-    //     expect(res.return_data).toBe(true)
-    //   });
-    // })
   });
 
   // ---------------------------------------------------------------
